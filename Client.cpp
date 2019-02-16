6b54bbc3beb15f01ed761cc9d3cbae5798925de1
IrenaRistova <irena.ristova@hotmail.com>
2019-02-16 15:14:09 +0100

    commi

--- Client.cpp
+++ Client.cpp
@@ -3,7 +3,7 @@
 
 const std::string IP_ADDRESS = "52.58.97.202";
 const std::string PORT = "5378";
-
+// std::string result = " ";
 void Client::tick(){};
 
 int Client::readFromStdin()
@@ -18,13 +18,15 @@ int Client::readFromSocket()
 
 void Client::createSocketAndLogIn()
 {
-    //TODO
+
     addrinfo AddrInfo;
     AddrInfo.ai_family = AF_INET;
     AddrInfo.ai_socktype = SOCK_STREAM;
-    // objAddrInfo.ai_addr = ;
+    AddrInfo.ai_protocol = IPPROTO_TCP;
+    // AddrInfo.ai_addr = result;
+
     int getaddrinfo(const char IP_ADDRESS, const char PORT,
-                    const struct addrinfo *hints, struct addrinfo **res);
+                    const struct addrinfo AddrInfo, struct addrinfo **res);
 };
 
 void Client::closeSocket(){};
